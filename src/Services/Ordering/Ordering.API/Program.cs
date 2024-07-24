fad903ca0f0e0bbb33316bad2b98b2b70c6cbba8
TITUS THOMAS <titusthomas8@gmail.com>
2024-07-24 22:45:37 +0530

    Ordering Service initial
    Ordering Service initial

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,11 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices().AddInfrastructureServices(builder.Configuration).AddApiServices();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.UseApiServices();
 app.Run();
